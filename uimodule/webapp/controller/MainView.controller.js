60ab92eb4691440819d2cbd89106a55b369f2b52
lemaiwo <wouterlemaire120@hotmail.com>
2022-08-08 20:58:20 +0200

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define(
+    ["./BaseController"],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("be.wl.DemoCustomControlGenerator.controller.MainView", {
+            onInit: function () {},
+        });
+    }
+);
